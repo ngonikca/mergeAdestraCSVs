6add4ec2dbf8bcbee9fefa5af68fde9d0b5fe0b7
Nicolás Gonik <ngonik@consumeraffairs.com>
2018-12-13 14:24:20 -0300

    added headers and fixed some minor bugs

--- mergeAdestraCSV.py
+++ mergeAdestraCSV.py
@@ -1,28 +1,31 @@
-import os, csv
+import os, csv, datetime
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-
-print('Current dir: ' + dir_path)
-print('Files available to merge: ')
-
-if os.path.exists("merged.csv"):
-    os.remove("merged.csv")
-else:
-    print("merged.csv file does not exist")
+out_filename = 'merged-' + str(datetime.datetime.now()) + '.csv'
 
 os.chdir('files')
 
+if os.path.exists(".DS_STORE"):
+    os.remove(".DS_STORE")
+
+i = 0
 for file in os.listdir():
     print(file)
     filename = os.path.splitext(file)[0]
 
     with open(file, 'r') as current_csv:
         csvread = csv.reader(current_csv, delimiter=',')
-        next(csvread)
+        
+        if i != 0:
+            next(csvread) #saltamos el header si no es el primer archivo
+        
         for row in csvread:
             row.append(filename)
             row_final = ', '.join(row)
+
             #abrimos el csv final
-            with open(os.path.join(os.pardir, 'merged.csv'), 'a') as csvfile:
+            with open(os.path.join(os.pardir, out_filename), 'a') as csvfile:
                 csvfile.write(row_final)
                 csvfile.write('\n')
+        
+        i += 1
